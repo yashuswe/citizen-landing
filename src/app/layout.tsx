4f5a3240689ff936217c263cbf3bb8f657bf9ec7
Yashassvi Suhane <87795720+yashuswe@users.noreply.github.com>
2024-06-13 18:56:53 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Citizenwave",
+  title: " Citizenwave",
   description: "The wave that moves forward",
 };
 
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/logo-png.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

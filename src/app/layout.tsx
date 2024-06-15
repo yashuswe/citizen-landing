5fb615216626af9ab63d15f3b274c778f0878049
yashuswe <yashassvishipra@gmail.com>
2024-06-15 17:28:39 +0530

    ADD: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/assets/logo-png.png" />
+      <link rel="icon" href="/logo-png.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>

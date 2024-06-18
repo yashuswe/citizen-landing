1f0a95ab349eb097d51b6f94fa103aec9987a0a3
yashuswe <yashassvishipra@gmail.com>
2024-06-18 17:48:11 +0530

    UPDATE: AboutUs

--- src/app/sections/AboutUs/AboutUs.tsx
+++ src/app/sections/AboutUs/AboutUs.tsx
@@ -1,23 +1,37 @@
 import React from "react";
-import Arpan from "../../../../public/assets/arpan-sri.jpg";
-import Bala from "../../../../public/assets/bal.jpg";
 import Image from "next/image";
 import Title from "@/app/components/Title";
+import Arpan from "../../../../public/assets/arpan-sri.jpg";
+import Bala from "../../../../public/assets/bal.jpg";
+import Vivek from "../../../../public/assets/vivek.jpg";
 
 const AboutUs = () => {
   return (
     <div className="px-4">
       <Title id="about" text="About Us" />
-      <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center dark:text-gray-200 mb-10 px-10 italic">
-        Citizenwave Consulting is a political consulting and campaign management
-        company. The firm&apos;s expertise in digital campaigning, booth
-        management, reputation management, and surveys have been a game-changer
-        in the fiercely competitive political landscape. Founded by Priyadarsi
-        Bal, a seasoned stock market expert, and Arpan Srivastava, a prominent
-        digital entrepreneur, Citizen Wave leverages a unique blend of financial
-        insight and digital innovation to craft winning strategies. Their
-        leadership has been pivotal in the firm&apos;s ability to offer tailored
-        consultancy services that meet the specific needs of each candidate.
+      <p className="mt-6 text-lg leading-8 text-gray-800 sm:text-left dark:text-gray-200 mb-10 px-10 italic">
+        Welcome to Citizenwave Consulting, where we are dedicated to
+        revolutionizing the political consulting landscape in India. We provide
+        end-to-end political consulting services, including campaign strategy
+        and management, digital campaigns, messaging and communication,
+        booth-level management, and war room operations. Our team comprises
+        political experts with extensive experience, ensuring that every
+        campaign we handle is executed with precision and effectiveness. Our
+        mission is to empower political candidates with the strategies,
+        insights, and tools they need to succeed in elections, enhancing
+        democratic processes by ensuring every voice is heard and every vote
+        counts.
+        <br />
+        Citizenwave Consulting has successfully managed campaigns for multiple
+        Lok Sabha candidates across India and Vidhan Sabha candidates in Odisha.
+        A notable achievement includes the comprehensive management of Pravati
+        Parida's campaign in the Nimapara constituency, which played a crucial
+        role in her election as Deputy Chief Minister of Odisha. We combine
+        traditional political know-how with modern digital techniques to deliver
+        impactful results. As we continue to expand our reach and influence, we
+        invite you to join us in shaping the future of Indian politics. Contact
+        us today to learn more about how we can help you achieve your political
+        ambitions. Together, we can create a wave of change.
       </p>
       <div className="p-8">
         <Title id="founders" text="Founders" />
@@ -28,7 +42,7 @@ const AboutUs = () => {
             <div className="mt-12 md:mt-0">
               <Image
                 src={Bala}
-                alt="About Us Image"
+                alt="Priyadarsi Bal"
                 className="object-cover rounded-lg shadow-md"
               />
             </div>
@@ -64,22 +78,52 @@ const AboutUs = () => {
               </h3>
               <p className="mt-4 text-gray-600 text-lg">
                 Arpan Srivastava is a co-founder and Director at Citizenwave. He
-                is also the CEO of DigitalWins, one of India&apos;s leading
-                Martech companies with clients including Indian Terrain,
-                FabIndia, and Aditya Birla Group. In his prior role at Cadentic
-                Consulting Inc. in the US, he spearheaded sales, securing
-                partnerships with notable clients like IBM, Shell, and Verizon.
-                During his college days, Arpan founded Praan Industries,
-                specializing in reputation management for clients such as
-                Vedanta and Vodafone.
+                is also the CEO of DigitalWins, one of India's leading Martech
+                companies with clients including Indian Terrain, FabIndia, and
+                Aditya Birla Group. In his prior role at Cadentic Consulting
+                Inc. in the US, he spearheaded sales, securing partnerships with
+                notable clients like IBM, Shell, and Verizon. During his college
+                days, Arpan founded Praan Industries, specializing in reputation
+                management for clients such as Vedanta and Vodafone.
               </p>
             </div>
             <div className="mt-12 md:mt-0">
               <Image
                 src={Arpan}
-                alt="About Us Image"
+                alt="Arpan Srivastava"
                 className="object-cover rounded-lg shadow-md"
               />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-gray-100">
+        <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
+            <div className="mt-12 md:mt-0">
+              <Image
+                src={Vivek}
+                alt="Priyadarsi Bal"
+                className="object-cover rounded-lg shadow-md"
+                height={100}
+                width={800}
+              />
+            </div>
+            <div className="max-w-lg">
+              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+                Vivek Singh
+              </h2>
+              <h3 className="text-lg font-extrabold text-gray-900 sm:text-xl">
+                CEO
+              </h3>
+              <p className="mt-4 text-gray-600 text-lg">
+                Vivek Singh is a Political Anthropologist whose work focuses on
+                contextualizing the force and consequences of governance through
+                Time, Space, and Bodies. His research and writing are rooted in
+                international politics and cross the fields of anthropology,
+                indigenous studies, Civilization, Religion, Race, and Gender
+                studies, as well as Politics.
+              </p>
             </div>
           </div>
         </div>

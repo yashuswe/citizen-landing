7f6aa160b06558bde0017015da3d42ebb4d1427d
yashuswe <yashassvishipra@gmail.com>
2024-06-19 18:00:55 +0530

    UPDATE: Hero Section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/public/favicon.ico" sizes="any"/>
+      {/* <link rel="icon" type="image/png" href="/public/assests/image.png" sizes="any"/> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- src/app/sections/HeroSection/Hero.tsx
+++ src/app/sections/HeroSection/Hero.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import Carousel from './Carousel'; // Adjust the path as necessary
+import React from "react";
+import Carousel from "./Carousel"; 
 
 const Hero = () => {
   return (
     <div className="relative w-full h-[80vh]">
       <Carousel />
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-20">
-        <h1 className="text-white text-4xl font-bold animate-fadeIn font-serif">Your path to victory</h1>
+        <div className="flex h-2/3 w-2/3 bg-opacity-40 bg-gray-800">
+          <h1 className="text-white text-4xl font-extrabold py-40 px-72 animate-fadeIn font-serif">
+            Your path to victory
+          </h1>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
